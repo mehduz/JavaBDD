45c4ef43ac61c030147c91cf2cc1467919dd3b8a
nexus <PC@PC-PC>
2014-07-18 23:37:18 +0200

    new

--- JavaBDD/src/dal/dao/ClasseDao.java
+++ JavaBDD/src/dal/dao/ClasseDao.java
@@ -1,5 +1,12 @@
 package dal.dao;
 
+import beans.Eleve;
+import dal.DAOException;
+
 public interface ClasseDao {
 
+	 void creer( Classe classe ) throws DAOException;
+
+	 Classe trouver( String email ) throws DAOException;
+	
 }
