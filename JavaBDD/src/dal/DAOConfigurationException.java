2b52685366a0a9e2c5ca165e646b2dcefdbc70a4
mehduz <syccoruz@gmail.com>
2014-07-18 21:03:26 +0200

    DAOConfigurationException v1.0.0 implemented

---
+++ JavaBDD/src/dal/DAOConfigurationException.java
@@ -0,0 +1,16 @@
+package dal;
+
+public class DAOConfigurationException extends RuntimeException {
+
+	    public DAOConfigurationException( String message ) {
+	        super( message );
+	    }
+
+	    public DAOConfigurationException( String message, Throwable cause ) {
+	        super( message, cause );
+	    }
+
+	    public DAOConfigurationException( Throwable cause ) {
+	        super( cause );
+	    }
+}
