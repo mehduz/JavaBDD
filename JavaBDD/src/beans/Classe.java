e53331715e6505f4e497b8c0720d51b542d2f74a
SimonL <SimonL@Simon>
2014-07-18 22:29:13 +0200

    Commit - 22:28 - Getter/Setter + Constructor

--- JavaBDD/src/beans/Classe.java
+++ JavaBDD/src/beans/Classe.java
@@ -5,4 +5,19 @@ import java.util.Date;
 class Classe {
 	
 	private String Nom_classe;
+	
+	public Classe(String nom_classe) {
+		super();
+		Nom_classe = nom_classe;
+	}
+
+	public String getNom_classe() {
+		return Nom_classe;
+	}
+
+	public void setNom_classe(String nom_classe) {
+		Nom_classe = nom_classe;
+	}
+	
+	
 }
