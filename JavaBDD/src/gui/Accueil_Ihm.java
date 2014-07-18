a102ce80e30fa6ab5a9fda134578bfda3f37f5fb
n3ocat <rcourtieux01@gmail.com>
2014-07-18 22:36:51 +0200

    création ihm accueil

---
+++ JavaBDD/src/gui/Accueil_Ihm.java
@@ -0,0 +1,47 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Accueil_Ihm extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -684831082624221575L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Accueil_Ihm frame = new Accueil_Ihm();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Accueil_Ihm() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 300, 800);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
