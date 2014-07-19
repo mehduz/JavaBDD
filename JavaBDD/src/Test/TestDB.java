6f551fb4e191f71600cbf4bc48b223b2574f13b2
nexus <PC@PC-PC>
2014-07-19 15:30:25 +0200

    new

--- JavaBDD/src/Test/TestDB.java
+++ JavaBDD/src/Test/TestDB.java
@@ -16,7 +16,7 @@ public class TestDB {
 		EleveDaoImpl el = new EleveDaoImpl(new DAOFactory());
 		Eleve eleve = el.trouver("Romain", "5026bc63b5418ffdb54f238db245ec01");
 
-		System.out.println(eleve.getCode_postal());
+		System.out.println(eleve);
 		
 		fail("Not yet implemented");
 	}
