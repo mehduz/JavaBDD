7d2a24e0eb26fe726351190d1f593ed3ab9bd066
nexus <PC@PC-PC>
2014-07-19 19:59:30 +0200

    new

--- JavaBDD/src/dal/daoImpl/ClasseDaoImpl.java
+++ JavaBDD/src/dal/daoImpl/ClasseDaoImpl.java
@@ -7,6 +7,12 @@ import dal.dao.ClasseDao;
 
 public class ClasseDaoImpl extends SuperDaoImpl implements ClasseDao  {
 
+	 private static final String SQL_SELECT_PAR_LOGIN_MDP = "SELECT ID_personne FROM Authentication WHERE Login = ? and MDP = ?";
+	 private static final String SQL_INSERT_ELEVE = "INSERT INTO eleve (Date_naissance, Ville_naissance, Pays_naissance,Sexe,"+
+			 "Date_inscription, Etablissement_prec, Photo, Rue,"+
+			 "Code_postal, Ville, Email,Remarques_medicale, ID_personne, Nom_classe,"+
+			 " ID_contact, ID_prof) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	
 	public ClasseDaoImpl(DAOFactory daoFactory) {
 		super(daoFactory);
 		// TODO Auto-generated constructor stub
--- JavaBDD/src/dal/daoImpl/ProfesseurDaoImpl.java
+++ JavaBDD/src/dal/daoImpl/ProfesseurDaoImpl.java
@@ -1,12 +1,25 @@
 package dal.daoImpl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import beans.Eleve;
 import beans.Professeur;
+import dal.DAODataBaseManager;
 import dal.DAOException;
 import dal.DAOFactory;
 import dal.dao.ProfesseurDao;
 
 public class ProfesseurDaoImpl extends SuperDaoImpl implements ProfesseurDao {
 
+	
+	 private static final String SQL_SELECT_PAR_LOGIN_MDP = "SELECT ID_personne FROM Authentication WHERE Login = ? and MDP = ?";
+	 private static final String SQL_SELECT_PROFESSEUR_PAR_ID_PERSONNE = "SELECT * FROM Professeur, Personne WHERE Professeur.ID_personne = ? AND Professeur.ID_personne = Personne.ID_personne ";
+			
+	
 	public ProfesseurDaoImpl(DAOFactory daoFactory) {
 		super(daoFactory);
 		// TODO Auto-generated constructor stub
@@ -24,4 +37,54 @@ public class ProfesseurDaoImpl extends SuperDaoImpl implements ProfesseurDao {
 		return null;
 	}
 
+	public Professeur trouver(String login, String mdp) throws DAOException {
+		
+		Connection connexion = null;
+	    PreparedStatement preparedStatement = null;
+	    ResultSet resultSet = null;
+	    Professeur professeur = null;
+
+	    try {
+	        /* Récupération d'une connexion depuis la Factory */
+	        connexion = daoFactory.getConnection();
+	        preparedStatement = DAODataBaseManager.initialisationRequetePreparee( connexion, SQL_SELECT_PAR_LOGIN_MDP, false, login, mdp);
+	        resultSet = preparedStatement.executeQuery();
+	        /* Parcours de la ligne de données de l'éventuel ResulSet retourné */
+	        if ( resultSet.next() ) {
+
+	        	  preparedStatement = DAODataBaseManager.initialisationRequetePreparee( connexion, SQL_SELECT_PROFESSEUR_PAR_ID_PERSONNE, false, resultSet.getInt("ID_personne"));
+	        	  resultSet = preparedStatement.executeQuery();
+			     
+	        	  if ( resultSet.next() ) {
+	        		  
+	        		  professeur = map( resultSet );
+	        		   
+	        	  }
+	        	
+	        	
+	        }
+	    } catch ( SQLException e ) {
+	        throw new DAOException( e );
+	    } finally {
+	    	DAODataBaseManager.fermeturesSilencieuses( resultSet, preparedStatement, connexion );
+	    }
+
+	    return professeur;
+		
+	}
+	
+	private static Professeur map( ResultSet resultSet ) throws SQLException {
+		Professeur professeur = new Professeur();
+
+		professeur.setEmail(resultSet.getString("Email"));
+		professeur.setNom(resultSet.getString("Nom"));
+		professeur.setPrenom(resultSet.getString("Prenom"));
+		professeur.setTel_domicile(resultSet.getInt("Tel_domicile"));
+		professeur.setTel_mobile(resultSet.getInt("Tel_mobile"));
+		professeur.setID_prof(resultSet.getInt("ID_prof"));
+		
+	    return professeur;
+	}
+	
+	
 }
