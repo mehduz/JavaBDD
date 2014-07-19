aba26bc2ebbade33095b35471f6b51a2f8206cff
n3ocat <rcourtieux01@gmail.com>
2014-07-19 21:46:32 +0200

    ihm

--- JavaBDD/src/gui/JTable.java
+++ JavaBDD/src/gui/JTable.java
@@ -1,8 +1,63 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
-public class JTable {
+import java.awt.Desktop;
+import java.awt.Font;
+import java.awt.Dimension;
 
-		  
+import javax.swing.JSeparator;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+public class JTable extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -684831082624221575L;
+
+	/**
+	 * Create the frame.
+	 */
+	public JTable() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("University Manager");
+		setSize(new Dimension(1280, 720));
+		setResizable(false);
+		setVisible(true);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(322, 20, 932, 651);
+		getContentPane().add(panel);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon(Ihm_Eleve.class.getResource("/gui/ressources/fond_principal.jpg")));
+		lblNewLabel_3.setBounds(300, 0, 974, 691);
+		getContentPane().add(lblNewLabel_3);
+
+	}
+	
+	public void setPanelIdentification(String login, String profil) {
+		textField.setText(login);
+		textField_1.setText(profil);
+	}
 }
