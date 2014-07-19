0ab78180d898a6893818fc645efdcd31c085dca5
nexus <PC@PC-PC>
2014-07-20 00:27:05 +0200

    new

--- JavaBDD/src/dal/daoImpl/MatiereDaoImpl.java
+++ JavaBDD/src/dal/daoImpl/MatiereDaoImpl.java
@@ -1,20 +1,56 @@
 package dal.daoImpl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
+
 import beans.Matiere;
+import dal.DAODataBaseManager;
 import dal.DAOException;
 import dal.DAOFactory;
 import dal.dao.MatiereDao;
  
 public class MatiereDaoImpl  extends SuperDaoImpl implements MatiereDao {
 
+	 private static final String SQL_INSERT_MATIERE = "INSERT INTO matiere (Nom_matiere) VALUES (?)";
+		
+	
 	public MatiereDaoImpl(DAOFactory daoFactory) {
 		super(daoFactory);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void creer(Matiere matiere) throws DAOException {
+	public int creer(Matiere matiere) throws DAOException {
 		// TODO Auto-generated method stub
+
+
+		Connection connexion = null;
+	    PreparedStatement preparedStatement = null;
+	    
+	    try {
+	        /* Récupération d'une connexion depuis la Factory */
+	        connexion = daoFactory.getConnection();
+
+	  	  		 preparedStatement = DAODataBaseManager.initialisationRequetePreparee( connexion, SQL_INSERT_MATIERE, true, 
+	  	  				matiere.getNom_matiere());
+	  	  
+	  	   int statut = preparedStatement.executeUpdate();
+	        /* Analyse du statut retourné par la requête d'insertion */
+	        if ( statut == 0 ) {
+	            throw new DAOException( "Échec de la création de l'utilisateur, aucune ligne ajoutée dans la table." );
+	        }
+	        
+	    
+	        return 1;
+	        
+	    } catch ( SQLException e ) {
+	        throw new DAOException( e );
+	    } finally {
+	    	DAODataBaseManager.fermeturesSilencieuses( preparedStatement, connexion );
+	    }
 		
 	}
 
