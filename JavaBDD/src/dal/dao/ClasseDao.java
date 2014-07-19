543eecba1d89a5c932dcbcc7d2bc973baebeb34a
nexus <PC@PC-PC>
2014-07-20 00:19:25 +0200

    new

--- JavaBDD/src/dal/dao/ClasseDao.java
+++ JavaBDD/src/dal/dao/ClasseDao.java
@@ -5,7 +5,7 @@ import dal.DAOException;
 
 public interface ClasseDao {
 
-	 void creer( Classe classe ) throws DAOException;
+	 int creer( Classe classe ) throws DAOException;
 
 	 Classe trouver( String nom_classe ) throws DAOException;
 	
