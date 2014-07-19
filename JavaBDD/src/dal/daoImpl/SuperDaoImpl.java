aef4c1d22be1e9c822b78a835503024af088b928
nexus <PC@PC-PC>
2014-07-19 15:58:47 +0200

    new

---
+++ JavaBDD/src/dal/daoImpl/SuperDaoImpl.java
@@ -0,0 +1,15 @@
+package dal.daoImpl;
+
+import dal.DAOFactory;
+
+public class SuperDaoImpl  {
+
+	 private DAOFactory   daoFactory;
+	 
+	  public SuperDaoImpl( DAOFactory daoFactory ) {
+	        this.daoFactory = daoFactory;
+	    }
+	 
+	
+	
+}
