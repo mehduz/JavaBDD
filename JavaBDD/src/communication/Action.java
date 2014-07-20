fd456cbe027572acd040d9972f96469fedd1cebb
n3ocat <rcourtieux01@gmail.com>
2014-07-20 13:08:36 +0200

    communication updated

--- JavaBDD/src/communication/Action.java
+++ JavaBDD/src/communication/Action.java
@@ -1,5 +1,23 @@
 package communication;
 
+import javax.xml.ws.Response;
+
 public abstract class Action {
+	
+	private Message message;
+
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+
+	protected Action(Message message) {
+		this.message = message;
+	}
+
 	public abstract void execute();
+	public abstract Reponse getReponse();
 }
