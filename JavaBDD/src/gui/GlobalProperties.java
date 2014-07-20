b5032b32bb9d8a481dfbd9343c32aa1eece60b38
mehduz <syccoruz@gmail.com>
2014-07-20 15:10:52 +0200

    properties

--- JavaBDD/src/gui/GlobalProperties.java
+++ JavaBDD/src/gui/GlobalProperties.java
@@ -1,9 +1,11 @@
 package gui;
+import java.util.ArrayList;
 import java.util.Dictionary;
 
 public class GlobalProperties {
 		
 	private static Dictionary<String, Object> properties;
+	private static ArrayList<String> ValidKeys;
 	
 	public static void registerProperty(String key, Object value){
 		properties.put(key, value);
