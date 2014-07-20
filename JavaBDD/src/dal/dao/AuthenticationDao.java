f274bfa2bc43ad880ad5f0f19d4b22b4f539642e
nexus <PC@PC-PC>
2014-07-20 14:50:17 +0200

    adding authentication pattern

--- JavaBDD/src/dal/dao/AuthenticationDao.java
+++ JavaBDD/src/dal/dao/AuthenticationDao.java
@@ -1,7 +1,9 @@
-//package dal.dao;
-//
-//public interface AuthenticationDao {
-//	
-//	Authentication trouver(String login, String mdp);
-//
-//}
+package dal.dao;
+
+import beans.Authentication;
+
+public interface AuthenticationDao {
+	
+	Authentication trouver(String login, String mdp);
+
+}
