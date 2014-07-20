c7c7cdf106b2c0ec59d50477ffd547792a062192
nexus <PC@PC-PC>
2014-07-20 02:46:33 +0200

    new

--- JavaBDD/src/dal/dao/MatiereDao.java
+++ JavaBDD/src/dal/dao/MatiereDao.java
@@ -1,5 +1,7 @@
 package dal.dao;
 
+import java.util.ArrayList;
+
 import beans.Matiere;
 import dal.DAOException;
 
@@ -8,6 +10,8 @@ public interface MatiereDao {
 	int creer( Matiere classe ) throws DAOException;
 
 	Matiere trouver( String nom_matiere ) throws DAOException;
+
+	ArrayList<Matiere> getAll() throws DAOException;
 	
 
 }
