3b8a0973421780800396b020d356d318c0a21ccc
nexus <PC@PC-PC>
2014-07-20 02:49:30 +0200

    yo

--- JavaBDD/src/dal/dao/ClasseDao.java
+++ JavaBDD/src/dal/dao/ClasseDao.java
@@ -1,5 +1,7 @@
 package dal.dao;
 
+import java.util.ArrayList;
+
 import beans.Classe;
 import dal.DAOException;
 
@@ -8,5 +10,7 @@ public interface ClasseDao {
 	 int creer( Classe classe ) throws DAOException;
 
 	 Classe trouver( String nom_classe ) throws DAOException;
+
+	ArrayList<Classe> getAll() throws DAOException;
 	
 }
